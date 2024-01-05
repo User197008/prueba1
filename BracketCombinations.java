0698aa122cc8fb5a6c2a8e2aac1a3e0ff040a1b7
User197008 <155701320+User197008@users.noreply.github.com>
2024-01-05 13:17:06 +0900

    Update BracketCombinations.java

--- BracketCombinations.java
+++ BracketCombinations.java
@@ -1,1 +1,22 @@
+class Main{
+  public static int BracketCombinations(int n) {
+    
+    return (int)(factorial(n * 2) / (factorial(n + 1) * factorial(n)));
+}
 
+    public static double factorial(int n) {
+		double fact = 1;
+		for (int i = 1; i <= n; i++) {
+			fact *= i;
+		}
+		return fact;
+	}  
+    
+    public static void main (String[] args) {  
+    
+    int num = 3;
+    
+    System.out.println(BracketCombinations(num));
+  }    
+  
+}
